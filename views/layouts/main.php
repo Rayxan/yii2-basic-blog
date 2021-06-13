244fbd6ec919b4203ee8a03734940ec6c2885303
Raylan Sales <Raylanwork@gmail.com>
2021-06-13 01:16:13 -0300

    Removed Contact and About from the navbar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,8 +39,6 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => [Yii::$app->homeUrl]],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
